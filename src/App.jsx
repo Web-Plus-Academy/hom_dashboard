f94ed52f14bb33bca88962851d711ea78a076f10
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-07-27 10:32:05 +0530

    logout3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
@@ -10,51 +10,46 @@ import POD from './Components/POD/POD';
 import Login from './Components/Login/Login';
 import TaskSubmit from './Components/TaskSubmit/TaskSubmit';
 import axios from './axiosConfig';
-// import { useNavigate } from 'react-router-dom';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [adminDetails, setAdminDetails] = useState(null);
-  // const navigate = useNavigate();
 
   useEffect(() => {
-    const authState = localStorage.getItem('isAuthenticated');
+    const authState = localStorage.getItem('auth');
     const adminDetails = localStorage.getItem('adminDetails');
-    if (authState) {
+    if (authState && adminDetails) {
       setIsAuthenticated(JSON.parse(authState));
       setAdminDetails(JSON.parse(adminDetails));
     }
   }, []);
 
   const handleLogin = (authState) => {
-    localStorage.setItem('isAuthenticated', JSON.stringify(authState));
+    localStorage.setItem('auth', JSON.stringify(authState));
     const adminDetails = JSON.parse(localStorage.getItem('adminDetails'));
     setIsAuthenticated(authState);
     setAdminDetails(adminDetails);
   };
 
   const handleLogout = async () => {
-    try{
-      
-      const response = await axios.get('/api/admin/logoutAdmin' );
-      
-      if(response.data.success){
+    try {
+      const response = await axios.get('/api/admin/logoutAdmin');
+
+      if (response.data.success) {
         toast.success(response.data.message);
-        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('auth');
         localStorage.removeItem('adminDetails');
         setIsAuthenticated(false);
         setAdminDetails(null);
-        // navigate('/');
-      }
-      else{
+      } else {
         toast.error(response.data.message);
       }
-  }catch(err){
-    console.log(err);
-  }
+    } catch (err) {
+      console.log(err);
+      toast.error('An error occurred. Please try again later.');
+    }
+  };
 
-}
-    
   return (
     <Router>
       <ToastContainer />
@@ -62,7 +57,7 @@ const App = () => {
         <div className='application'>
           <Navbar onLogout={handleLogout} />
           <div className="app-container">
-            <Sidebar adminDetails={adminDetails}  />
+            <Sidebar adminDetails={adminDetails} />
             <div className="main-content">
               <Routes>
                 <Route path="/" element={<Navigate to="/pod" />} />
